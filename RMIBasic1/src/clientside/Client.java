ac9208bef1c4be9166ecad30e9be000d1fbcc6b4
FELLIPE ADORNO <FELLIPE ADORNO@192.168.0.9>
2015-01-05 22:13:17 -0200

    Fazendo funcionar o GIT.

---
+++ RMIBasic1/src/clientside/Client.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientside;
+
+/**
+ *
+ * @author FELLIPE ADORNO
+ */
+public interface Client extends java.rmi.Remote {
+    public void alert(String msg) throws java.rmi.RemoteException;
+}
---
+++ RMIBasic1/src/serverside/Server.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serverside;
+
+/**
+ *
+ * @author FELLIPE ADORNO
+ */
+public interface Server extends java.rmi.Remote {
+    public boolean addClient(clientside.Client client) throws java.rmi.RemoteException;
+    public boolean removeClient(clientside.Client client) throws java.rmi.RemoteException;
+}
