2390416790e9ddc2691e7439a280c1c0820eb861
FELLIPE ADORNO <FELLIPE ADORNO@192.168.0.9>
2015-01-06 11:01:37 -0200

    Implementação básica.

---
+++ src/serverside/ServerRun.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serverside;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RMISecurityManager;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author FELLIPE ADORNO
+ */
+public class ServerRun {
+   
+    public static void main(String a[]) {
+        try {
+            System.setSecurityManager(new RMISecurityManager());
+            LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
+            ServerImpl obj = new ServerImpl();
+            Naming.rebind("server", obj);
+        } catch (RemoteException ex) {
+            Logger.getLogger(ServerRun.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(ServerRun.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
